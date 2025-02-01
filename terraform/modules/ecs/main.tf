4ba213c2a4eed76421d223d9aa2cf20ff340c70b
kapi3476343 <kapishparticle41@outlook.com>
2025-02-01 15:37:01 +0530

    Create main.tf

---
+++ terraform/modules/ecs/main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_container_group" "ecs" {
+  name                = var.ecs_cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  ip_address_type     = "private"
+  os_type             = "Linux"
+
+  container {
+    name   = "my-container"
+    image  = var.container_image
+    cpu    = "0.5"
+    memory = "1.5"
+  }
+}
