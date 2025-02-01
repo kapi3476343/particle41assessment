f3e1768098191e75502363d2d400ece0c32a746b
kapi3476343 <kapishparticle41@outlook.com>
2025-02-01 18:04:25 +0530

    Update outputs.tf

--- terraform/modules/ecs/outputs.tf
+++ terraform/modules/ecs/outputs.tf
@@ -2,3 +2,8 @@ output "ecs_service_name" {
   description = "Name of the ECS service"
   value       = azurerm_container_group.ecs.name
 }
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = azurerm_container_group.ecs.name
+}
